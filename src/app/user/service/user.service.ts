7e62540dcfc769ade4ea6b092e770abbda78a2ca
Erick <esgaspar@msn.com>
2023-10-23 23:18:29 -0300

    prod

--- src/app/user/service/user.service.ts
+++ src/app/user/service/user.service.ts
@@ -7,7 +7,7 @@ import { User } from '../model/user';
     providedIn: 'root'
 })
 export class UserService {
-    host = "http://indicadores.groundbreakable.com/"
+    host = "http://indicadores.groundbreakable.com/api/"
     // host = "http://89.117.32.90/api/";
     // host = "http://localhost:8080/api/";
     // host = "https://esgaspar.cloudns.ph/api/";
